84953f0fda038b488afb729ff8d19629ef9f0e72
whitbreezy <whitneybrady20@gmail.com>
2023-12-04 21:20:20 -0700

    sessions work

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,6 +17,7 @@ router.post('/', async (req, res) => {
 });
 //route works but redirects don't
 router.post('/login', async (req, res) => {
+  
   try {
     // Find the user who matches the posted e-mail address
     const userData = await User.findOne({ where: { email: req.body.email } });
